c8860012639a82cc10dbed9fc131a3a013717286
Elvis Reyes <teclaelvis01@gmail.com>
2024-09-07 00:38:39 +0000

    feat: :zap: Actualizar estilos de código y configuración de formato

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: 'rgb(98, 68, 197)'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
